483dee829ea745ac2e9ae4ff45e187a6f474a672
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-07-25 20:22:16 -0600

    One Call

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
-function weatherApiCall(){
-    fetch(https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key)
-}
- weather-function
-function(){
-    }
+var searchHistory = []
+var prevCitySearched = ""
 
-var requestUrl = '';
-
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data)
-
-
-
-    L.mapquest.key = 'KEY';
-var baseLayer = L.mapquest.tileLayer('dark');
-          
-L.mapquest.geocoding().geocode(['New York, NY'], showMap);
-
-function showMap(err, data) {
-  var map = createMap();
-  map.addControl(L.mapquest.control());
-  addLayerControl(map);
-}
-
-function createMap() {
-  var map = L.mapquest.map('map', {
-    center: [40.7237, -73.9825],
-    zoom: 14,
-    layers: baseLayer
-  });
-  return map;
+var cityWeather = function(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=33a442ce0b1dad52f9352616c57d9d69&units=imperial";
+    var geoLocation = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=33a442ce0b1dad52f9352616c57d9d69"
+    fetch(geoLocation)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data);
+        getoneCall(data, city);
+    })
+};
+function getoneCall(data, city) {
+    var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&appid=33a442ce0b1dad52f9352616c57d9d69&units=imperial"
+    fetch(oneCall)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data);
+        displayWeather(data, city);
+        displayFiveDay(data);
+    })
 }
 
-function addLayerControl(map) {
-  L.control.layers({
-    'Map': L.mapquest.tileLayer('map'),
-    'Satellite': L.mapquest.tileLayer('satellite'),
-    'Hybrid': L.mapquest.tileLayer('hybrid'),
-    'Light': L.mapquest.tileLayer('light'),
-    'Dark': baseLayer
-  }, {}, { position: 'topleft'}).addTo(map);
-}
 
-fetchButton.addEventListener('click', getApi);
+var trailSearch = function(event) {
+  event.preventDefault();
 
+  
+  var trailName = $("#trailname").val().trim()
+
+  if (trailName) {
+      trailWeather(trailName);
+
+      $("#trailname").val("");
+
+  } else {
+      alert("Enter a trail name");
+  }
+};
+
+var displayWeather = function(weatherData, city) {
+  $("#main-trail-name").text
+  $("#main-trail-temp").text(`Temp: ${weatherData.current.temp}` + "°F");
+  $("#main-trail-humid").text(`Humidity: ${weatherData.current.humidity}` + "%");
+  $("#main-trail-wind").text(`Wind Speed: ${weatherData.current.wind_speed}` + " mph");
