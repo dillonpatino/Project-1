6e58e7605ccb02a5f8583ffe9b890e3222330491
Sejin <kisejin2015@gmail.com>
2022-07-25 20:58:54 -0600

    hello

--- script.js
+++ script.js
@@ -1,52 +1,60 @@
+var searchHistory = []
+var prevCitySearched = ""
+
+var cityWeather = function(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=33a442ce0b1dad52f9352616c57d9d69&units=imperial";
+    var geoLocation = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=33a442ce0b1dad52f9352616c57d9d69"
+    fetch(geoLocation)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data);
+        getoneCall(data, city);
+    })
+};
+function getoneCall(data, city) {
+    var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&appid=33a442ce0b1dad52f9352616c57d9d69&units=imperial"
+    fetch(oneCall)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data);
+        displayWeather(data, city);
+        displayFiveDay(data);
+    })
+
 function weatherApiCall(){
     var weatherKey = '{e2ca5eb2bd8c415361262e3d9a4b2aa5}'
     fetch(https://api.openweathermap.org/data/2.5/weather?lat=43.700111&lon=-79.416298&appid={e2ca5eb2bd8c415361262e3d9a4b2aa5})
-}
- weather-function
-function(){
-    }
+    
 
+}
+
+=======
 var requestUrl = '';
 var fetchButton = document.getElementById('search');
 
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data)
 
+var trailSearch = function(event) {
+  event.preventDefault();
 
+  
+  var trailName = $("#trailname").val().trim()
 
-    L.mapquest.key = 'KEY';
-var baseLayer = L.mapquest.tileLayer('dark');
-          
-L.mapquest.geocoding().geocode(['New York, NY'], showMap);
+  if (trailName) {
+      trailWeather(trailName);
 
-function showMap(err, data) {
-  var map = createMap();
-  map.addControl(L.mapquest.control());
-  addLayerControl(map);
-}
+      $("#trailname").val("");
 
-function createMap() {
-  var map = L.mapquest.map('map', {
-    center: [40.7237, -73.9825],
-    zoom: 14,
-    layers: baseLayer
-  });
-  return map;
-}
+  } else {
+      alert("Enter a trail name");
+  }
+};
 
-function addLayerControl(map) {
-  L.control.layers({
-    'Map': L.mapquest.tileLayer('map'),
-    'Satellite': L.mapquest.tileLayer('satellite'),
-    'Hybrid': L.mapquest.tileLayer('hybrid'),
-    'Light': L.mapquest.tileLayer('light'),
-    'Dark': baseLayer
-  }, {}, { position: 'topleft'}).addTo(map);
-}
-
-fetchButton.addEventListener('click', getApi);
-
+var displayWeather = function(weatherData, city) {
+  $("#main-trail-name").text
+  $("#main-trail-temp").text(`Temp: ${weatherData.current.temp}` + "°F");
+  $("#main-trail-humid").text(`Humidity: ${weatherData.current.humidity}` + "%");
+  $("#main-trail-wind").text(`Wind Speed: ${weatherData.current.wind_speed}` + " mph");
